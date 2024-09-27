5b96433588deb39a7af2b94df498b54663ee220c
Eva Pace <eba.pachi@gmail.com>
2024-09-27 00:18:19 -0300

    BEHOLD WEBSOCKETS IN C

--- src/main.c
+++ src/main.c
@@ -1,23 +1,21 @@
 #include <http.h>
 
-static void on_response(http_s *h);
+void on_open(ws_s *ws) {
+    fprintf(stderr, "ON OPENNN\n");
+    websocket_write(ws, (fio_str_info_s){.data = "{ \"type\": \"ping\" }", .len = 18}, 1);
+}
+void on_message(ws_s *ws, fio_str_info_s msg, uint8_t is_text) {
+    fprintf(stderr, "ON MESSAGE: %s\n", msg.data);
+}
+void on_shutdown(ws_s *ws) {
+    fprintf(stderr, "ON SHUTDOWN\n");
+}
 
 int main(int argc, char const *argv[]) {
-  http_connect("http://example.com/foo", NULL,
-               .on_response = on_response);
+  websocket_connect("localhost:6666",
+               .on_message = on_message,
+               .on_open = on_open,
+               .on_shutdown = on_shutdown);
   fio_start(.threads = 1);
   return 0;
 }
-
-static void on_response(http_s *h) {
-  if (h->status_str == FIOBJ_INVALID) {
-    /* first response is always empty, nothing was sent yet */
-    http_finish(h);
-    return;
-  }
-  /* Second response is actual response */
-  FIOBJ r = http_req2str(h);
-  fprintf(stderr, "%s\n", fiobj_obj2cstr(r).data);
-  fio_stop();
-}
-
